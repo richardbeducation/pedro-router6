981e493d9e83b2146d4b11818328f3ea11f92e71
richardbeducation <richardbeducation@gmail.com>
2022-07-11 16:26:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/:username' element={<Profile />} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>
       <div>Footer</div>
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function Profile () {
   let navigate = useNavigate();
+  let { username } = useParams()
   return (
-    <div>This is the PROFILE page 
+    <div>This is the PROFILE page for {username}! 
       <button onClick={() => {
         navigate('/about')
       }}>Change to the About page</button>
